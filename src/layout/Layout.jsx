8bd95ba35b2135c1d98f326369dec9f8ad529124
Ali Pirzadeh <ali2021pirzadeh@gmail.com>
2025-04-08 00:14:26 +0330

    style layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,7 +8,7 @@ function Layout({ children }) {
   const [state] = useCart();
   return (
     <Container>
-      <header className="flex sm:flex-row justify-between items-center bg-red-400 p-4 sm:p-4 rounded-2xl space-y-2 sm:space-y-0">
+      <header className="flex sm:flex-row justify-between items-center bg-red-400 p-4 sm:p-4 rounded-2xl space-y-2 sm:space-y-0 mx-7 shadow-2xl">
         <Link
           to="/products"
           className="text-lg sm:text-xl hover:text-amber-100"
